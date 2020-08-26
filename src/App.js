d21c4c6bd8ee29c4e412fb2c91302ae66647a3e9
Max <max@dietr.net>
2020-08-26 17:25:03 +0200

    Add table

--- src/App.js
+++ src/App.js
@@ -4,9 +4,26 @@ import './App.css';
 
 import {
   Button,
+  Paper,
   Slider,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
   Typography
 } from '@material-ui/core';
+
+function createData(name, sets, reps, kgs) {
+  return { name, sets, reps, kgs };
+}
+
+const rows = [
+  createData('Chest press', 2, 10, 25),
+  createData('Chest press', 2, 15, 15),
+  createData('Chest press', 1, 20, 40),
+];
 
 function App() {
   return (
@@ -54,6 +71,32 @@ function App() {
         />
       </div>
       <Button color="primary" variant="contained" size="large">Add</Button>
+      <div className="table">
+        <TableContainer component={Paper}>
+          <Table size="small" aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Exercise</TableCell>
+                <TableCell align="right">Sets</TableCell>
+                <TableCell align="right">Reps</TableCell>
+                <TableCell align="right">Kgs</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.sets}</TableCell>
+                  <TableCell align="right">{row.reps}</TableCell>
+                  <TableCell align="right">{row.kgs}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
       <div className="icon-author">Icons made by <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
     </div>
   );
