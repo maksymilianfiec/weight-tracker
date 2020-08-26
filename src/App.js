0f8fe139e271e12db594b8725766bbaf83cf53f0
Max <max@dietr.net>
2020-08-26 16:25:53 +0200

    Add sliders

--- src/App.js
+++ src/App.js
@@ -2,14 +2,58 @@ import React from 'react';
 import logo from './sport.svg';
 import './App.css';
 
-import { Button } from '@material-ui/core';
+import {
+  Button,
+  Slider,
+  Typography
+} from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
-      
-      <Button color="primary" variant="contained" size="large">OK</Button>
+      <Typography variant="h5" component="h1" gutterBottom>
+        Chest press
+      </Typography>
+      <div className="sliders">
+        <Typography gutterBottom>
+            Sets
+        </Typography>
+        <Slider
+            defaultValue={3}
+            aria-labelledby="discrete-slider-small-steps"
+            step={1}
+            marks
+            min={1}
+            max={6}
+            valueLabelDisplay="on"
+        />
+        <Typography gutterBottom>
+            Reps
+        </Typography>
+        <Slider
+            defaultValue={10}
+            aria-labelledby="discrete-slider-small-steps"
+            step={1}
+            marks
+            min={1}
+            max={20}
+            valueLabelDisplay="on"
+        />
+        <Typography gutterBottom>
+            Kgs
+        </Typography>
+        <Slider
+            defaultValue={30}
+            aria-labelledby="discrete-slider-small-steps"
+            step={2.5}
+            marks
+            min={2.5}
+            max={60}
+            valueLabelDisplay="on"
+        />
+      </div>
+      <Button color="primary" variant="contained" size="large">Add</Button>
       <div className="icon-author">Icons made by <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
     </div>
   );
