e2d1aea54c34f1a147c886ec94741b3bc1e2b828
Max <max@dietr.net>
2020-08-28 15:44:06 +0200

    Add grid and refactor styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
 import logo from './sport.svg';
-import './App.css';
 
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import {
+  makeStyles,
+  Box,
   Button,
+  Grid,
   Paper,
   Slider,
   Table,
@@ -18,16 +20,19 @@ import {
 } from '@material-ui/core';
 
 function App() {
+  const classes = useStyles();
   const [reps, setReps] = useState(10);
   const [kgs, setKgs] = useState(0);
   const [exercise, setExercise] = useState('');
   const [rows, setRows] = useState([]);
   const exercises = [
-    'Chest press',
     'Biceps curl',
     'Chest fly',
+    'Chest press',
+    'Lateral raise',
+    'Plank hold',
+    'Raised leg hold',
     'Standing curl',
-    'Lateral raise'
   ];
 
   function updateReps(event, value) {
@@ -57,82 +62,156 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <Autocomplete
-        inputValue={exercise}
-        onInputChange={(event, newInputValue) => {updateExercise(newInputValue)}}
-        options={exercises}
-        freeSolo
-        style={{ width: '85%', maxWidth: 640, marginBottom: 30 }}
-        renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
-      />
-      <div className="sliders">
-        <Typography gutterBottom>
-            Reps
-        </Typography>
-        <Slider
-            value={reps}
-            aria-labelledby="discrete-slider-small-steps"
-            step={1}
-            marks
-            min={1}
-            max={20}
-            valueLabelDisplay="on"
-            onChange={updateReps}
-        />
-        <Typography gutterBottom>
-            Kgs
-        </Typography>
-        <Slider
-            value={kgs}
-            aria-labelledby="discrete-slider-small-steps"
-            step={0.25}
-            marks
-            min={0}
-            max={50}
-            valueLabelDisplay="on"
-            onChange={updateKgs}
-        />
-      </div>
-      <div>
-        <Button color="primary" variant="text" size="large" onClick={clearSliders}>Clear</Button>
-        <Button color="primary" variant="contained" size="large" onClick={addToTable}>Log</Button>
-      </div>
-      <div className="table">
-        <Typography variant="h5" component="h2" gutterBottom>
-          History
-        </Typography>
-        {rows.length > 0 ?
-          <TableContainer component={Paper}>
-            <Table size="small" aria-label="a dense table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Exercise</TableCell>
-                  <TableCell align="right">Reps</TableCell>
-                  <TableCell align="right">Kgs</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row, index) => (
-                  <TableRow key={row.name + index}>
-                    <TableCell component="th" scope="row">
-                      {row.name}
-                    </TableCell>
-                    <TableCell align="right">{row.reps}</TableCell>
-                    <TableCell align="right">{row.kgs}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        :
-          <Typography>Nothing to display yet</Typography>
-        }
-      </div>
-      <div className="icon-author">Icons made by <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-    </div>
+    <Box className={classes.root}>
+      {/* ------------------------ LOGO ------------------------ */}
+      <img src={logo} className={classes.logo} alt="logo" />
+      <Grid container>
+        {/* ------------------------ LEFT SIDE ------------------------ */}
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h5" component="h1" gutterBottom>
+            Log exercise
+          </Typography>
+          <Paper className={classes.inputPanel}>
+            <Box className={classes.inputPanelInner}>
+              <Autocomplete
+                inputValue={exercise}
+                onInputChange={(event, newInputValue) => {updateExercise(newInputValue)}}
+                options={exercises}
+                freeSolo
+                className={classes.autocomplete}
+                renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
+              />
+              <Box className={classes.sliders}>
+                <Typography gutterBottom>
+                    Reps
+                </Typography>
+                <Slider
+                    value={reps}
+                    aria-labelledby="discrete-slider-small-steps"
+                    step={1}
+                    marks
+                    min={1}
+                    max={20}
+                    valueLabelDisplay="on"
+                    onChange={updateReps}
+                />
+                <Typography gutterBottom>
+                    Kgs
+                </Typography>
+                <Slider
+                    value={kgs}
+                    aria-labelledby="discrete-slider-small-steps"
+                    step={0.25}
+                    marks
+                    min={0}
+                    max={50}
+                    valueLabelDisplay="on"
+                    onChange={updateKgs}
+                />
+              </Box>
+              <Box className={classes.buttons}>
+                <Button color="primary" variant="text" size="large" onClick={clearSliders}>Clear</Button>
+                <Button color="primary" variant="contained" size="large" onClick={addToTable}>Log</Button>
+              </Box>
+            </Box>
+          </Paper>
+        </Grid>
+        {/* ------------------------ RIGHT SIDE ------------------------ */}
+        <Grid item xs={12} sm={6}>
+          
+            <Typography variant="h5" component="h1" gutterBottom>
+              History
+            </Typography>
+            {rows.length > 0 ?
+              <TableContainer component={Paper} className={classes.historyPanel}>
+                <Table size="small" aria-label="a dense table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Exercise</TableCell>
+                      <TableCell align="right">Reps</TableCell>
+                      <TableCell align="right">Kgs</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((row, index) => (
+                      <TableRow key={row.name + index}>
+                        <TableCell component="th" scope="row">
+                          {row.name}
+                        </TableCell>
+                        <TableCell align="right">{row.reps}</TableCell>
+                        <TableCell align="right">{row.kgs}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            :
+              <Typography variant="body2">Nothing to display yet</Typography>
+            }
+          
+        </Grid>
+      </Grid>
+      {/* ------------------------ FOOTER ------------------------ */}
+      <Typography className={classes.footer}>Icons made by <a className={classes.footerLinks} href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a className={classes.footerLinks} href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></Typography>
+    </Box>
   );
 }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // flexGrow: 1,
+    position: 'relative',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  logo: {
+    height: '10vh',
+    margin: '5vh',
+  },
+  inputPanel: {
+    width: '90%',
+    padding: theme.spacing(3),
+    margin: '0 auto 30px',
+    display: 'flex',
+    flexDirection:'column',
+    alignItems: 'center',
+  },
+  inputPanelInner: {
+    width: '90%',
+    maxWidth: 640,
+  },
+  autocomplete: {
+    width: '100%',
+    marginBottom: 50,
+  },
+  sliders: {
+    width: '100%',
+    marginBottom: 30,
+    textAlign: 'left',
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  historyPanel: {
+    width: '90%',
+    margin: '0 auto 30px',
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 'center',
+    color: 'lightgrey',
+    fontSize: 10,
+  },
+  footerLinks: {
+    color: 'lightgrey',
+    textDecoration: 'none',
+  }
+  
+}));
 
 export default App;
