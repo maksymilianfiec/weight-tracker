ee71e181ef18c1fd2d62233d9bd9d3f8a835e480
Max <max@dietr.net>
2020-08-31 15:44:09 +0200

    Add tabs and duration slider

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import {
   IconButton,
   Paper,
   Slider,
+  Tab,
+  Tabs,
   Table,
   TableBody,
   TableCell,
@@ -25,8 +27,10 @@ function App() {
   const classes = useStyles();
   const [reps, setReps] = useState(10);
   const [kgs, setKgs] = useState(0);
+  const [duration, setDuration] = useState(120);
   const [exercise, setExercise] = useState('');
   const [rows, setRows] = useState([]);
+  const [tabValue, setTabValue] = useState(0);
   const exercises = [
     'Biceps curl',
     'Chest fly',
@@ -45,6 +49,10 @@ function App() {
     setKgs(value);
   }
 
+  function updateDuration(event, value) {
+    setDuration(value);
+  }
+
   function updateExercise(value) {
     setExercise(value);
   }
@@ -54,18 +62,43 @@ function App() {
   }
 
   function addToTable() {
-    setRows([...rows, createData(exercise, reps, kgs)]);
+    tabValue === 0
+    ? setRows([...rows, createData(exercise, `${reps} reps`, kgs!==0 ? kgs : '-')])
+    : setRows([...rows, createData(exercise, `${duration / 60} min`, '-')]);
   }
 
   function clearSliders() {
     setExercise('');
     setReps(10);
     setKgs(0);
+    setDuration(120);
   }
 
   function deleteRow(index) {
     setRows([...rows.slice(0, index), ...rows.slice(index+1)]);
   }
+
+  function TabPanel(props) {
+    const { children, value, index } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={index}
+      >
+        {value === index && (
+          <Box>
+            {children}
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+  const changeTab = (event, newValue) => {
+    setTabValue(newValue);
+  };
 
   return (
     <Box className={classes.root}>
@@ -79,6 +112,10 @@ function App() {
           </Typography>
           <Paper className={classes.inputPanel}>
             <Box className={classes.inputPanelInner}>
+              <Tabs value={tabValue} onChange={changeTab} aria-label="Reps(0) or Duration(1)">
+                <Tab label="Reps" id={0} />
+                <Tab label="Duration" id={1} />
+              </Tabs>
               <Autocomplete
                 inputValue={exercise}
                 onInputChange={(event, newInputValue) => {updateExercise(newInputValue)}}
@@ -87,34 +124,53 @@ function App() {
                 className={classes.autocomplete}
                 renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
               />
-              <Box className={classes.sliders}>
-                <Typography gutterBottom>
-                    Reps
-                </Typography>
-                <Slider
-                    value={reps}
-                    aria-labelledby="discrete-slider-small-steps"
-                    step={1}
-                    marks
-                    min={1}
-                    max={20}
-                    valueLabelDisplay="on"
-                    onChange={updateReps}
-                />
-                <Typography gutterBottom>
-                    Kgs
-                </Typography>
-                <Slider
-                    value={kgs}
-                    aria-labelledby="discrete-slider-small-steps"
-                    step={0.25}
-                    marks
+              <TabPanel value={tabValue} index={0}>
+                <Box className={classes.sliders}>
+                  <Typography gutterBottom>
+                      Reps
+                  </Typography>
+                  <Slider
+                      value={reps}
+                      aria-labelledby="discrete-slider-small-steps"
+                      step={1}
+                      marks
+                      min={1}
+                      max={20}
+                      valueLabelDisplay="on"
+                      onChange={updateReps}
+                  />
+                  <Typography gutterBottom>
+                      Kgs
+                  </Typography>
+                  <Slider
+                      value={kgs}
+                      aria-labelledby="discrete-slider-small-steps"
+                      step={0.25}
+                      marks
+                      min={0}
+                      max={50}
+                      valueLabelDisplay="on"
+                      onChange={updateKgs}
+                  />
+                </Box>
+              </TabPanel>
+              <TabPanel value={tabValue} index={1}>
+                <Box className={classes.sliders}>
+                  <Typography gutterBottom>
+                    Duration
+                  </Typography>
+                  <Slider
+                    value={duration}
+                    aria-labelledby="non-linear-slider"
+                    step={15}
                     min={0}
-                    max={50}
+                    max={600}
+                    scale={(x) => x / 60}
                     valueLabelDisplay="on"
-                    onChange={updateKgs}
-                />
-              </Box>
+                    onChange={updateDuration}
+                  />
+                </Box>
+              </TabPanel>
               <Box className={classes.buttons}>
                 <Button color="primary" variant="text" size="large" onClick={clearSliders}>Clear</Button>
                 <Button color="primary" variant="contained" size="large" onClick={addToTable} disabled={exercise ==='' ? true : false}>Log</Button>
@@ -134,7 +190,7 @@ function App() {
                   <TableHead>
                     <TableRow>
                       <TableCell>Exercise</TableCell>
-                      <TableCell align="right">Reps</TableCell>
+                      <TableCell align="right">Set</TableCell>
                       <TableCell align="right">Kgs</TableCell>
                       <TableCell align="right"></TableCell>
                     </TableRow>
@@ -194,10 +250,12 @@ const useStyles = makeStyles((theme) => ({
   },
   autocomplete: {
     width: '100%',
-    marginBottom: 50,
+    marginTop: 15,
+    marginBottom: 15,
   },
   sliders: {
     width: '100%',
+    marginTop: 30,
     marginBottom: 30,
     textAlign: 'left',
   },
